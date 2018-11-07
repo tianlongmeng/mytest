ee749bb9b1e73da88cd229319fff3ce0f5eb4822
long <772683966@qq.com>
2018-11-07 20:58:50 +0800

    develop分支第一次修改

--- day66-git/src/main/java/com/itheima/user.java
+++ day66-git/src/main/java/com/itheima/user.java
@@ -3,6 +3,6 @@ package com.itheima;
 public class user {
     public static void main(String[] args) {
         System.out.println("XiuXiu");
-		System.out.println("DaXiuXiu");
+		System.out.println("XiaoXiuXiu");
     }
 }
