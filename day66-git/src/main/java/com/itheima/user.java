f2b7a7f7060a908ae5f76295f147e997b40dd718
long <772683966@qq.com>
2018-11-07 21:01:26 +0800

    第二次修改

--- day66-git/src/main/java/com/itheima/user.java
+++ day66-git/src/main/java/com/itheima/user.java
@@ -4,5 +4,6 @@ public class user {
     public static void main(String[] args) {
         System.out.println("XiuXiu");
 		System.out.println("DaXiuXiu");
+		System.out.println("CheXiuXiu");
     }
 }
